40bc6a63cb23f7816e374ca998ed312c59577c63
Aloeverras <edouard.bouche@gmail.com>
2026-06-05 08:34:45 +0000

    base of loop game pong

--- src/adaptaters/PygameRunnerPongGameAdaptater.py
+++ src/adaptaters/PygameRunnerPongGameAdaptater.py
@@ -34,10 +34,13 @@ class PygameRunnerPongGameAdaptater(RunnerPongGamePort):
         
         
         while True:
+            
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                    
+                    
             touche = pygame.key.get_pressed()
             
             if touche[pygame.K_UP] and player1.top > 0:
@@ -50,4 +53,39 @@ class PygameRunnerPongGameAdaptater(RunnerPongGamePort):
                 player2.y -= palette_speed    
                 
             if touche[pygame.K_s] and player2.bottom < height:
-                player2.y += palette_speed      
+                player2.y += palette_speed  
+                
+            ball.x += speed_ball_x
+            ball.y += speed_ball_y
+            
+            if ball.top <= 0 or ball.bottom >= height:
+                speed_ball_y *= -1 
+                
+            if ball.collidedict(player1) or ball.collidedict(player2):
+                speed_ball_x *= -1    
+                
+                
+            if ball.left <= 0:
+                scrore_player1 += 1
+                ball.center = (width // 2, height // 2)  
+                speed_ball_x *= -1      
+                
+            if ball.right <= 0:
+                scrore_player2 += 1
+                ball.center = (width // 2, height // 2)  
+                speed_ball_x *= -1         
+                
+            screen.fill(black)    
+            pygame.draw.rect(screen, white, player1)
+            pygame.draw.rect(screen, white, player2)
+            pygame.draw.ellipse(screen, white, ball)
+            pygame.draw.aaline(screen, white, (width // 2.0), (height // 2.0))
+            
+            player1_text = police.render(str(scrore_player1), True, white)
+            screen.blit(player1_text, (width // 2 + 20, 20))
+            
+            player2_text = police.render(str(scrore_player2), True, white)
+            screen.blit(player1_text, (width // 2 - 50, 20))
+            
+            pygame.display.flip()
+            time.tick(60)
