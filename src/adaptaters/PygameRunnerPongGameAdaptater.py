16a9b97dab5f79e892b17f9dabcea94628fe83ef
Aloeverras <edouard.bouche@gmail.com>
2026-06-05 07:54:12 +0000

    base pygame

--- src/adaptaters/PygameRunnerPongGameAdaptater.py
+++ src/adaptaters/PygameRunnerPongGameAdaptater.py
@@ -4,5 +4,11 @@ from domaine.ports.RunnerPongGamePort import RunnerPongGamePort
 class PygameRunnerPongGameAdaptater(RunnerPongGamePort):
     
     def start_runner_pong_game(self):
+        
         pygame.init()
         
+        white = (255, 255, 255)
+        black = (0, 0, 0)
+        
+        screen = pygame.display.set_mode((800, 800))
+        pygame.display.set_caption("Pong")
