b7e4b3fa7bdec382a45faeebbbba49edd3214ca0
Aloeverras <edouard.bouche@gmail.com>
2026-06-05 08:36:27 +0000

    try catch in service runner

--- src/domaine/services/RunnerService.py
+++ src/domaine/services/RunnerService.py
@@ -5,6 +5,12 @@ from domaine.ports.RunnerPongGamePort import RunnerPongGamePort
 class RunnerService:
     runner : RunnerPongGamePort
     
+    def start(self):
+        try:
+            self.runner.start_runner_pong_game()
+        except Exception as e:
+            print(e)    
+    
     @property
     def get_runner_pong_game(self) -> RunnerPongGamePort:
         return self.runner
