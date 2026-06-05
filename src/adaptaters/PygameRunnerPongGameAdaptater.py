e5e7142372c34ea2aba50b339abe7ea776907260
Aloeverras <edouard.bouche@gmail.com>
2026-06-05 07:47:04 +0000

    add colors in runner adaptater

--- src/adaptaters/PygameRunnerPongGameAdaptater.py
+++ src/adaptaters/PygameRunnerPongGameAdaptater.py
@@ -5,3 +5,5 @@ class PygameRunnerPongGameAdaptater(RunnerPongGamePort):
     
     def start_runner_pong_game(self):
         pygame.init()
+        white = (255, 255, 255)
+        black = (0, 0, 0)
